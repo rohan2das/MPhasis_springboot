eea0366ce4a617b1bafd550ed80e839cd7f63032
rohan2das <dasrohan20000@gmail.com>
2025-03-12 12:49:45 +0530

    till break-SpringJDBC CRUD operation

--- SpringJDBC/src/main/java/com/test/dao/CustDao.java
+++ SpringJDBC/src/main/java/com/test/dao/CustDao.java
@@ -1,7 +1,15 @@
 package com.test.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jspecify.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 
 import com.test.model.Customer;
 
@@ -24,4 +32,31 @@ public class CustDao {
 				+ "' WHERE id =" + cst.getId();
 		jdbcTemplate.execute(sql);
 	}
+	
+	public void deleteCustomer(Customer cst) {
+		String sql = "Delete from customer where id= "+cst.getId()+";";
+		
+		jdbcTemplate.execute(sql);
+	}
+	
+	public List<Customer> getAllCustomers(){
+		return jdbcTemplate.query("select * from customer", new ResultSetExtractor<List<Customer>>() {
+
+			@Override
+			public @Nullable List<Customer> extractData(ResultSet rs) throws SQLException, DataAccessException {
+				List<Customer> listData = new ArrayList<>();
+				
+				while(rs.next()) {
+					Customer obj = new Customer();
+					obj.setId(rs.getInt(1));
+					obj.setName(rs.getString(2));
+					obj.setType(rs.getString(3));
+					listData.add(obj);
+				}				
+				return listData;
+			}
+			
+		});
+	}
+	
 }
--- SpringJDBC/src/main/java/com/test/main/StoreData.java
+++ SpringJDBC/src/main/java/com/test/main/StoreData.java
@@ -1,7 +1,10 @@
 package com.test.main;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.test.dao.CustDao;
 import com.test.model.Customer;
@@ -9,22 +12,34 @@ import com.test.model.Customer;
 public class StoreData {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
-		
-		Customer obj = new Customer();
-		obj.setName("Virat");
-		obj.setType("Preferred");
-		
 		CustDao cst = ctx.getBean("custDao", CustDao.class);
+		
+//		Customer obj = new Customer();
+//		obj.setName("Axar");
+//		obj.setType("AllRounder");
+//		
 //		cst.createCustomer(obj);
+//		System.out.println("done creating in db");
+
 		
 		
-		Customer objUpdate = new Customer();
-		obj.setName("Rahul");
-		obj.setType("visiting");
-		obj.setId(2);
-		cst.updateCustomer(objUpdate);
+//		Customer objUpdate = new Customer();
+//		objUpdate.setId(2);
+//		objUpdate.setName("Rahul");
+//		objUpdate.setType("visiting");
+//		cst.updateCustomer(objUpdate);
+//		System.out.println("done updating in db");
+	
 		
-		System.out.println("done updating in db");
+//		Customer objDelete = new Customer();
+//		objDelete.setId(5);
+//		cst.updateCustomer(objDelete);
+//		System.out.println("done deleting in db");
+		
+		
+		List<Customer> data = cst.getAllCustomers();
+		System.out.println(data);
+		data.forEach(cust->System.out.println(cust));
 		
 	}
 }
