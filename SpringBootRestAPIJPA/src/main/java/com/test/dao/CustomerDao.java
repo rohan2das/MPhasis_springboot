4eab107f9c0a42bbd514dc3776fc5f83d93cc2f1
rohan2das <dasrohan20000@gmail.com>
2025-03-14 13:05:20 +0530

    added SpringBootRestAPIJPA-CRUD operation on sql database

---
+++ SpringBootRestAPIJPA/src/main/java/com/test/dao/CustomerDao.java
@@ -0,0 +1,16 @@
+package com.test.dao;
+
+import java.util.List;
+
+import com.test.entity.Customer;
+
+public interface CustomerDao {
+
+	public Customer createCustomer(Customer cst);
+	
+	public List<Customer> readAllCustomers();
+	
+	public List<Customer> updateCustomer(Customer cst);
+	
+	public List<Customer> deleteCustomer(int id);
+}
